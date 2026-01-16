1f9f7ca562d1f703396c651581d1fd2094af940a
Nandanamrutiya18 <nandanamrutiya18@gmail.com>
2026-01-16 16:57:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
-def add(a, b):
-    return a + b
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "POC Python App deployed using Jenkins CI/CD 🚀"
 
 if __name__ == "__main__":
-    print("App is running")
+    app.run(host="0.0.0.0", port=5000)
